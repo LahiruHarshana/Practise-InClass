f18ae87c515d41a922f16d38a3af6acd2889a705
LahiruHarshana <lharshana2002@gmail.com>
2024-01-05 12:32:46 +0530

    updated

--- src/main/java/ijse/lk/practiseinclass/practiseinclass/HelloServlet.java
+++ src/main/java/ijse/lk/practiseinclass/practiseinclass/HelloServlet.java
@@ -10,6 +10,17 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        String name = request.getParameter("name");
+        String id = request.getParameter("id");
+        String address = request.getParameter("address");
+        Double salary = Double.parseDouble(request.getParameter("salary"));
+
+        System.out.println("Name: " + name);
+        System.out.println("Id: " + id);
+        System.out.println("Address: " + address);
+        System.out.println("Salary: " + salary);
+
         response.setContentType("text/html");
 
         PrintWriter out = response.getWriter();
