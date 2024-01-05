469fcceb3b360b2cff8aa336c58bc651217d0a08
LahiruHarshana <lharshana2002@gmail.com>
2024-01-05 11:05:20 +0530

    updated

--- src/main/java/ijse/lk/practiseinclass/practiseinclass/HelloServlet.java
+++ src/main/java/ijse/lk/practiseinclass/practiseinclass/HelloServlet.java
@@ -14,7 +14,7 @@ public class HelloServlet extends HttpServlet {
 
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
+        out.println("<h1> </h1>");
         out.println("</body></html>");
     }
 
